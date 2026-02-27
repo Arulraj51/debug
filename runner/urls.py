471a7b890a6d979deecba81549aaa74cabfd8784
Arulraj51 <arulrajarul5757@gmail.com>
2026-02-27 16:07:42 +0530

    update

--- runner/urls.py
+++ runner/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+from django.contrib import admin
 urlpatterns = [
     path('', views.index, name='index'),
     path('challenge/<int:id>/', views.challenge_view, name='challenge'),
+    path('admin/', admin.site.urls),    
 ]
